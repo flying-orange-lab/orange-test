6369a9df562c5d8083332100fca375b5187a07cf
flying-orange <flyingorange9463@gmail.com>
2026-03-07 12:57:25 +0900

    Feat/ 폼이 있는 포켓몬 오류 수정

--- src/app/services/move.service.ts
+++ src/app/services/move.service.ts
@@ -8,10 +8,42 @@ export class PokemonMoveService {
 
   getMoveFromPokemonKeyname(pokemonKeyname: string) {
     const moveDatas = this.dataHandleService.moveDatas;
+    if (!moveDatas) {
+      return {
+        learn: [],
+        tm: [],
+        tutor: [],
+      };
+    }
+
+    // 포켓몬의 기본 keyname
+    const base_pokemonKeyname = pokemonKeyname.split('-')[0] + '-0';
+
+    let current_move_learn = moveDatas.learn?.[pokemonKeyname] ?? [];
+    if (current_move_learn.length === 0) {
+      current_move_learn = moveDatas.learn?.[base_pokemonKeyname] ?? [];
+    }
+
+    let current_move_tm = moveDatas.tm?.[pokemonKeyname] ?? [];
+    if (current_move_tm.length === 0) {
+      current_move_tm = moveDatas.tm?.[base_pokemonKeyname] ?? [];
+    }
+
+    let current_move_tutor = moveDatas.tutor?.[pokemonKeyname] ?? [];
+    if (current_move_tutor.length === 0) {
+      current_move_tutor = moveDatas.tutor?.[base_pokemonKeyname] ?? [];
+    }
+
+    console.log(pokemonKeyname);
+    console.log(base_pokemonKeyname);
+    console.log(current_move_learn);
+    console.log(current_move_tm);
+    console.log(current_move_tutor);
+
     return {
-      learn: moveDatas?.learn?.[pokemonKeyname] ?? [],
-      tm: moveDatas?.tm?.[pokemonKeyname] ?? [],
-      tutor: moveDatas?.tutor?.[pokemonKeyname] ?? [],
+      learn: current_move_learn,
+      tm: current_move_tm,
+      tutor: current_move_tutor,
     };
   }
 }
