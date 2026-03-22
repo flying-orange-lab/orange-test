ace39d7fee08b7a89959c1f2f3cdfe4ae4c23eaa
flying-orange <flyingorange9463@gmail.com>
2026-03-22 11:38:15 +0900

    Fix/ 모달 수정

--- src/app/menu/pokedex/pokemon-move/pokemon-move.component.ts
+++ src/app/menu/pokedex/pokemon-move/pokemon-move.component.ts
@@ -1,5 +1,6 @@
 import { Component, inject, Input, OnInit } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatTabsModule } from '@angular/material/tabs';
 import { MoveLearnData, MoveTutorData } from 'src/app/models/move.model';
 import { DataHandleService } from 'src/app/services/data-handle.service';
 import { MoveDetailService } from 'src/app/services/move-detail.service';
@@ -14,11 +15,11 @@ export interface PokemonMoves {
 
 @Component({
   selector: 'app-pokemon-move',
+  imports: [MatTabsModule],
   templateUrl: './pokemon-move.component.html',
   styleUrl: './pokemon-move.component.less',
 })
 export class PokemonMoveComponent implements OnInit {
-  private dataHandleService = inject(DataHandleService);
   private pokemonMoveService = inject(PokemonMoveService);
   private moveDetailService = inject(MoveDetailService);
   private dialog = inject(MatDialog);
@@ -26,12 +27,6 @@ export class PokemonMoveComponent implements OnInit {
   @Input() pokemonKeyname!: string;
 
   moves?: PokemonMoves;
-  isRootDrawerOpen = true;
-  isDrawerOpen = {
-    learn: false,
-    tm: false,
-    tutor: false,
-  };
 
   ngOnInit() {
     this.moves = this.pokemonMoveService.getMoveFromPokemonKeyname(
@@ -42,35 +37,10 @@ export class PokemonMoveComponent implements OnInit {
   openMoveCard(moveName: string) {
     const moveDetail = this.moveDetailService.getMoveDetailByName(moveName);
     if (!moveDetail) return;
+
     this.dialog.open(MoveCardModalComponent, {
       data: { moveDetail },
       panelClass: 'move-card-modal-panel',
     });
-  }
-
-  toggleRootDrawer(event: Event) {
-    this.isRootDrawerOpen = !this.isRootDrawerOpen;
-    if (this.isRootDrawerOpen) {
-      this.scrollToDrawer(event);
-    }
-  }
-
-  toggleDrawer(event: Event, drawerName: 'learn' | 'tm' | 'tutor'): void {
-    this.isDrawerOpen[drawerName] = !this.isDrawerOpen[drawerName];
-    if (this.isDrawerOpen[drawerName]) {
-      this.scrollToDrawer(event);
-    }
-  }
-
-  scrollToDrawer(event: Event) {
-    setTimeout(() => {
-      const element = (event.target as HTMLElement).closest('.drawer-box');
-      if (element) {
-        const yOffset = -90;
-        const y =
-          element.getBoundingClientRect().top + window.scrollY + yOffset;
-        window.scrollTo({ top: y, behavior: 'smooth' });
-      }
-    }, 0);
   }
 }
