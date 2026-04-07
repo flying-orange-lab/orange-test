731ec0dd09a38ea58849492fd505624b2f699b32
flying-orange <flyingorange9463@gmail.com>
2026-04-07 20:09:49 +0900

    Fix/ 버전 4.12 스프라이트 오류 수정

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
   spriteBaseUrl:
-    'https://raw.githubusercontent.com/flying-orange-lab/alternative-black-cdn/main/assets/data-4.1',
+    'https://raw.githubusercontent.com/flying-orange-lab/alternative-black-cdn/main/assets/data-4.12',
 };
